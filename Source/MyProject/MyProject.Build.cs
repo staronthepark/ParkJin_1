f65e427296e4d7bbe9fdd34e2164621be328dab9
staronthepark <asdffg6934@naver.com>
2023-12-20 12:44:16 +0900

    Add Module

--- Source/MyProject/MyProject.Build.cs
+++ Source/MyProject/MyProject.Build.cs
@@ -8,6 +8,6 @@ public class MyProject : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput", "NavigationSystem", "AIModule", "UMG", "GameplayTasks", "LevelSequence", "MovieScene", "MediaAssets", "Niagara", "EnhancedInput" });
 	}
 }
